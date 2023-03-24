8e0691b691edac232cb94e9505b47a0148caba06
Nelson-Appiah-Agyei <nelsonappiah.agyei@gmail.com>
2023-03-24 04:42:26 -0700

    sags

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - prints those integers
+ * @i: represents any integer
+ * Return: always 0;
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n" , i);
+	else
+		printf("%d is positive\n", i);
+}
+
