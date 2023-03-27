11a242753f8b825206b093bd60095514950f2fc0
Nelson-Appiah-Agyei <nelsonappiah.agyei@gmail.com>
2023-03-27 16:00:38 -0700

    asrgkaks

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swapping integers
+ * @a: integer
+ * @b: fslkfsfg
+ * @m: kaflw
+ */
+
+
+void swap_int(int *a, int *b)
+{
+	int m;
+	m = *a;
+	*a = *b;
+	*b = m;
+}
