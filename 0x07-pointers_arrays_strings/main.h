56c5246023248dfdd2f9459445c742c4972462e2
Nelson-Appiah-Agyei <nelsonappiah.agyei@gmail.com>
2023-04-03 18:38:20 -0700

    fgkak

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
-
+void print_chessboard(char (*a)[8]);
 #endif 
