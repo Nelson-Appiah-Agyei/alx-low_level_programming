1aa65a1bd2c0ce72d451c8cd2795cf0d40ead34b
Nelson-Appiah-Agyei <nelsonappiah.agyei@gmail.com>
2023-04-10 09:57:25 -0700

    sfgaks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - We are in argc and argv 
+ * @argc: it is called the argument count 
+ * @argv: it is named as the argument vector
+ * Return: Always 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is about arguments in C
