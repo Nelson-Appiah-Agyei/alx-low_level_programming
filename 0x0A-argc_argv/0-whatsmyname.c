e7f410d4a7ab974e774e1f7f126d1b8972cdcf8b
Nelson-Appiah-Agyei <nelsonappiah.agyei@gmail.com>
2023-04-10 13:11:43 -0700

    sfkgal

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,30 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
-		
-#include "main.h"
-		
 
-		
 /**
-		
- * main - prints the name of the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
+ * main- a Nelson  program that prints its name
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
  * Return: Always 0 (Success)
-		
- */
-		
-int main(int argc __attribute__((unused)), char *argv[])
-		
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-		
-	printf("%s\n", *argv);
-		
+	int i = 0;
 
-		
+	printf("%s\n", argv[i]);
 	return (0);
-		
 }
-
